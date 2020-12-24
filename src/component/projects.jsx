285f71f6bc06cf26641eeee5bf3ebd47bdaf520e
Arpit Paliwal <apaliwal@localhost.localdomain>
2020-12-25 01:29:30 +0530

    portfolio initial commit

---
+++ src/component/projects.jsx
@@ -0,0 +1,264 @@
+import React from "react";
+import githubJob from "../img/github_jobs_react_app.png";
+import pixabay from "../img/pixabay_image_search.png";
+import beachResort from "../img/Beach_resort_reservation.png";
+import PropTypes from "prop-types";
+
+export default class Projects extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				<div id="my-projects" className="container-fluid bg-light mt-5">
+					<div className=" project-container container bg-light pb-5">
+						<h1
+							id="projects"
+							className={"text-center " + this.props.fadeInRight}>
+							Projects
+						</h1>
+						<div className="row my-5">
+							<div
+								className={
+									"testbg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>Git hub jobs search</h2>
+									</div>
+									<div>
+										<button
+											id="vmarine-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var marineModal = document.getElementById(
+													"vmarine"
+												);
+												modalBG.style.display = "block";
+												marineModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"aguabg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>Pixabay image finder</h2>
+									</div>
+									<div>
+										<button
+											id="agua-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var aguaModal = document.getElementById(
+													"agualuz"
+												);
+												modalBG.style.display = "block";
+												aguaModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"todobg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>Beach resort reservation</h2>
+									</div>
+									<div>
+										<button
+											id="todo-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var todoModal = document.getElementById(
+													"todolist"
+												);
+												modalBG.style.display = "block";
+												todoModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				{/*hidden modals*/}
+				<div id="gallery-card">
+					<div id="vmarine" className="modal-card">
+						<div className="visual">
+							<img src={githubJob} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>Github Job Search</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										Prototype marine web-application to
+										provide information about jobs search
+										using github apis.
+									</li>
+									<li>
+										Designed with ReactJs
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a
+									href="https://github-jobs-search-react1.herokuapp.com/"
+									target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var marineModal = document.getElementById(
+											"vmarine"
+										);
+										modalBG.style.display = "none";
+										marineModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="agualuz" className="modal-card">
+						<div className="visual">
+							<img src={pixabay} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>Pixabay image finder</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										Prototype website to find images using
+										pixabay api using React framework
+									</li>
+									<li>
+										Developed with HTML5, CSS3, React
+										
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a
+									href="https://pixaby-image-search-react.herokuapp.com/"
+									target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var aguaModal = document.getElementById(
+											"agualuz"
+										);
+										modalBG.style.display = "none";
+										aguaModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="todolist" className="modal-card">
+						<div className="visual">
+							<img src={beachResort} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>Beach resort reservation</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										A beach resort reservation full web application
+									</li>
+									<li>
+										Developed with ReactJS.
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a href="https://react-hotel-reservation-app.herokuapp.com/" target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var todoModal = document.getElementById(
+											"todolist"
+										);
+										modalBG.style.display = "none";
+										todoModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+Projects.propTypes = {
+	bounceIn: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
