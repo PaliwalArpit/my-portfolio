83bd5bf346fb6f0c3bc47bffd1e77ae7eab369e9
Arpit Paliwal <apaliwal@localhost.localdomain>
2020-12-25 01:34:57 +0530

    built

---
+++ build/precache-manifest.8a4d977ad510843e5284664fbb363324.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849601c9c75cd63d12c63a23025e9ab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "27705dc89330d6b01a10",
+    "url": "/static/css/main.a7b2978e.chunk.css"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/static/js/2.3f72f909.chunk.js"
+  },
+  {
+    "revision": "27705dc89330d6b01a10",
+    "url": "/static/js/main.351d6e1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f2e9f45e3286cb2814963317513ac5c",
+    "url": "/static/media/Beach_resort_reservation.9f2e9f45.png"
+  },
+  {
+    "revision": "74903ec83df614b2dc27a2f923cf6a6a",
+    "url": "/static/media/github_jobs_react_app.74903ec8.png"
+  },
+  {
+    "revision": "f761d0c4f8b37bbac2b275d6d2d81a2f",
+    "url": "/static/media/myImage.f761d0c4.png"
+  },
+  {
+    "revision": "840f50eefe1f5700f47bfc7672695309",
+    "url": "/static/media/pixabay_image_search.840f50ee.png"
+  }
+]);
